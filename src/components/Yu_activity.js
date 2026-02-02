943fd3903382660b4fddea7d4cc972e84c9759fc
Yu-Nakano0723 <8425079@ed.tus.ac.jp>
2026-02-02 22:30:46 +0900

    js

--- src/components/Yu_activity.js
+++ src/components/Yu_activity.js
@@ -1,14 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-  new Swiper('.swiper', {
-    loop: true,
-    speed: 1000,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  effect: 'fade',
+  speed: 1000,
+  autoplay: {
+    delay: 3000
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
+
